25acf3233e34ca9f9488bf34e5ce7e6fb8367e56
AyusshM <ayush.manghirmalani@gmail.com>
2020-05-02 23:02:47 +0530

    Added mongoose support

--- index.js
+++ index.js
@@ -1,56 +1,104 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var router = express.Router();
-
 configureExpress();
 
-const jsonObject = {
-    "ni3mumbaikar": "https://www.github.com/ni3mumbaikar",
-    "test": "https://www.github.com/ni3mumbaikar"
-};
+mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
+const urlSchema = new mongoose.Schema({
+  uid: String,
+  url: String
+});
+const Url = mongoose.model("Url", urlSchema);
+var redirectUrl = "";
+const testURL = new Url({
+  uid: 'test',
+  url: 'https://www.google.in'
+})
+testURL.save();
+const jsonObject = {
+  "ni3mumbaikar": "https://www.github.com/ni3mumbaikar",
+  "test": "https://www.github.com/ni3mumbaikar",
+  "r57fl0": "github_url_tera"
+};
 var request = require('request-promise');
 
-app.get('/', function (req, res) {
-    res.render('index', {success:false,code:''});
+app.get('/', function(req, res) {
+  res.render('index', {
+    success: false,
+    code: ''
+  });
+
 })
 
 app.get('/:id', (req, res) => {
-    const id = req.params.id;
-    if (jsonObject.hasOwnProperty(id)) {
-        res.redirect(jsonObject[id]);
-    } else
-        res.send(id);
+
+  const id = req.params.id;
+  Url.findOne({
+    uid: id
+  }, function(err, result) {
+    if (!err) {
+      redirectUrl = result.url.slice();
+      console.log(redirectUrl);
+      console.log(typeof redirectUrl);
+      console.log("inside");
+
+      res.redirect(redirectUrl);
+    }
+  });
+
+
+  console.log("outside");
+
+  // res.redirect("https://www.google.in");
+
+  // if (jsonObject.hasOwnProperty(id)) {
+  //     res.redirect(jsonObject[id]);
+  // } else
+  //     res.send(id);
 })
 
-app.post('/', function (req, res) {
-    console.log('posted url : ' + req.body.linkurl);
-    var code = generateUID();
-    jsonObject[code] = req.body.linkurl;
-    console.log(code);
-    res.render('index',{'success':true,code:code});
+app.post('/', function(req, res) {
+  console.log('posted url : ' + req.body.linkurl);
+  var code = generateUID();
+  const testurl = new Url({
+    uid: code,
+    url: req.body.linkurl
+  });
+  testurl.save();
+  jsonObject[code] = req.body.linkurl;
+  console.log(code);
+  flag = true;
+  res.render('index', {
+    'success': true,
+    code: code
+  });
 
 })
 
 function generateUID() {
-    // I generate the UID from two parts here 
-    // to ensure the random number provide enough bits.
-    var firstPart = (Math.random() * 46656) | 0;
-    var secondPart = (Math.random() * 46656) | 0;
-    firstPart = ("000" + firstPart.toString(36)).slice(-3);
-    secondPart = ("000" + secondPart.toString(36)).slice(-3);
-    return firstPart + secondPart;
+  // I generate the UID from two parts here
+  // to ensure the random number provide enough bits.
+  var firstPart = (Math.random() * 46656) | 0;
+  var secondPart = (Math.random() * 46656) | 0;
+  firstPart = ("000" + firstPart.toString(36)).slice(-3);
+  secondPart = ("000" + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
 }
 
 function configureExpress() {
-    app.use(express.static('public'));
-    app.use(express.static(__dirname + '/public'));
-    app.set('view engine', 'ejs');
-    app.use(bodyParser.json()); // support json encoded bodies
-    app.use(bodyParser.urlencoded({
-        extended: false
-    })); // support encoded bodies    
+  app.use(express.static('public'));
+  app.use(express.static(__dirname + '/public'));
+  app.set('view engine', 'ejs');
+  app.use(bodyParser.json()); // support json encoded bodies
+  app.use(bodyParser.urlencoded({
+    extended: true
+  })); // support encoded bodies
 }
 
 app.listen(process.env.PORT || 3000, () => console.log('Server is up and running'));
